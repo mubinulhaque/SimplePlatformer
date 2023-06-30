845b7e0e04a857b4cf83efb247af415e4097db36
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2023-06-30 12:20:01 +0100

    Added wall jumping
    Added the player to jump off walls by being close to a wall mid-air and moving away from the wall. Some lines have been rearranged to keep lines under 80 characters and enhance readability.

--- player.gd
+++ player.gd
@@ -6,17 +6,30 @@ extends CharacterBody2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 
 var double_jump : bool = false
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") # Get the gravity from the project settings to be synced with RigidBody nodes
+# Get the gravity from the project settings to be synced with RigidBody nodes
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") 
 
 func _physics_process(delta):
 	# Add the gravity
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.gravity_scale * delta
+		
+	# Handle wall jumping
+	if is_on_wall():
+		var wall_normal = get_wall_normal()
+		if (
+				not is_on_floor()
+				and ((Input.is_action_just_pressed("ui_left") and wall_normal == Vector2.LEFT)
+				or (Input.is_action_just_pressed("ui_right") and wall_normal == Vector2.RIGHT))
+		):
+			velocity.x = wall_normal.x * movement_data.speed
+			velocity.y = movement_data.jump_velocity
 
-	# Handle jumping
+	# Handle normal jumping
 	if is_on_floor(): double_jump = true
 	if Input.is_action_just_pressed("ui_accept"):
-		if (is_on_floor() or coyote_jump_timer.time_left > 0.0): # If the player is on the floor or has just left the floor
+		if (is_on_floor() or coyote_jump_timer.time_left > 0.0):
+			# If the player is on the floor or has just left the floor
 			velocity.y = movement_data.jump_velocity
 		elif double_jump: # If the player is mid-air and can double jump
 			velocity.y = movement_data.jump_velocity
@@ -27,9 +40,11 @@ func _physics_process(delta):
 	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
 	if horizontal_direction: # If the player wants to move horizontally
 		if is_on_floor(): # If the player wants to move horizontally while on the ground
-			velocity.x = move_toward(velocity.x, horizontal_direction * movement_data.speed, movement_data.acceleration * delta)
+			velocity.x = move_toward(velocity.x, horizontal_direction * movement_data.speed,
+					movement_data.acceleration * delta)
 		else: # If the player wants to move horizontally while mid-air
-			velocity.x = move_toward(velocity.x, horizontal_direction * movement_data.speed, movement_data.air_acceleration * delta)
+			velocity.x = move_toward(velocity.x, horizontal_direction * movement_data.speed,
+					movement_data.air_acceleration * delta)
 		sprite.flip_h = (horizontal_direction < 0)
 		sprite.play("run")
 	elif is_on_floor(): #Decelerate the player via friction if the player is on the floor
@@ -40,12 +55,13 @@ func _physics_process(delta):
 		sprite.play("jump")
 	
 	# Change the sprite animation to be jumping if in the air
-	# This has to be after the movement code, otherwise the "run" animation can ovveride the animation
+	# This has to be after the movement code,
+	# otherwise the "run" animation can ovveride the animation
 	if not is_on_floor(): sprite.play("jump")
 		
 	# Move while accounting for Coyote Time
 	var was_on_floor = is_on_floor()
 	move_and_slide()
-	var just_left_ground = was_on_floor and not is_on_floor() and velocity.y >= 0 # Has the player just left the ground and now falling
-	if just_left_ground:
+	var just_left_ground = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ground: # Has the player just left the ground and now falling
 		coyote_jump_timer.start()
